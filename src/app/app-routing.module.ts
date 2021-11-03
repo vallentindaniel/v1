f036f8b3048726ad4ea04cc873ec1bce24641978
vallentindaniel <49476775+vallentindaniel@users.noreply.github.com>
2021-11-03 17:45:25 +0200

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { RoadmapComponent } from './components/pages/roadmap/roadmap.component';
+import { SocialMediaComponent } from './components/pages/social-media/social-media.component';
+import { TokenomicsComponent } from './components/pages/tokenomics/tokenomics.component';
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { WhitepaperComponent } from './components/pages/whitepaper/whitepaper.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'Tokenomics', component: TokenomicsComponent },
+  { path: 'Whitepaper', component: WhitepaperComponent },
+  { path: 'Roadmap', component: RoadmapComponent },
+  { path: 'Social-Media', component: SocialMediaComponent },
+  { path: 'About-Us', component: AboutUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { TokenomicsComponent } from './components/pages/tokenomics/tokenomics.component';
+import { RoadmapComponent } from './components/pages/roadmap/roadmap.component';
+import { SocialMediaComponent } from './components/pages/social-media/social-media.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { WhitepaperComponent } from './components/pages/whitepaper/whitepaper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    WelcomeComponent,
+    FooterComponent,
+    TokenomicsComponent,
+    RoadmapComponent,
+    SocialMediaComponent,
+    AboutUsComponent,
+    WhitepaperComponent
   ],
   imports: [
     BrowserModule,
